2900807a17321277db53055c2b9b04293072c396
KelvinL98 <69175682+KelvinL98@users.noreply.github.com>
2020-08-17 10:42:29 +1000

    Update selectIO.py

--- venv/CarrierModulation/selectIO.py
+++ venv/CarrierModulation/selectIO.py
@@ -10,3 +10,15 @@ numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
             print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name')
+        
+       
+# select device by id
+#p.input_device_index = id
+#p.output_device_index 
+
+
+#add input devices to a list, kepeing device ID as its index,
+#add add items to a combo box using the addItems() method. 
+        #figure out how to handle combo box choices. 
+                #either use name and index from the initial list
+                #or figure out how to use combo boxes properly?
