75dc23b032600538a87edb86898ff8aad1fb198a
KelvinL98 <69175682+KelvinL98@users.noreply.github.com>
2020-10-30 14:58:33 +1100

    broken connections and links to functions

--- venv/Alans_Matlab/insertionDepthGUI.py
+++ venv/Alans_Matlab/insertionDepthGUI.py
@@ -23,7 +23,10 @@ from mm2hz import mm2hz
 from freqsFromCSV import freqsFromCSV
 
 
-class Ui_MainWindow(object):
+class Ui_MainWindow(QtWidgets.QMainWindow):
+    inputFile = ""
+    cfFile = ""
+    freqsFile = ""
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(210, 246)
@@ -34,22 +37,23 @@ class Ui_MainWindow(object):
         MainWindow.setSizePolicy(sizePolicy)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(20, 150, 151, 31))
-        self.pushButton.setObjectName("pushButton")
-        self.idepthverticalSlider = QtWidgets.QSlider(self.centralwidget)
-        self.idepthverticalSlider.setGeometry(QtCore.QRect(30, 40, 16, 101))
-        self.idepthverticalSlider.setOrientation(QtCore.Qt.Vertical)
-        self.idepthverticalSlider.setTickPosition(QtWidgets.QSlider.TicksBelow)
-        self.idepthverticalSlider.setTickInterval(0)
-        self.idepthverticalSlider.setObjectName("idepthverticalSlider")
+        self.playButton = QtWidgets.QPushButton(self.centralwidget)
+        self.playButton.setGeometry(QtCore.QRect(20, 150, 151, 31))
+        self.playButton.setObjectName("playButton")
+        self.iDepthSlider = QtWidgets.QSlider(self.centralwidget)
+        self.iDepthSlider.setGeometry(QtCore.QRect(30, 40, 16, 101))
+        self.iDepthSlider.setOrientation(QtCore.Qt.Vertical)
+        self.iDepthSlider.setTickPosition(QtWidgets.QSlider.TicksBelow)
+        self.iDepthSlider.setTickInterval(0)
+        self.iDepthSlider.setObjectName("iDepthSlider")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(50, 10, 81, 21))
         self.label.setScaledContents(True)
         self.label.setObjectName("label")
-        self.iDepthlineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.iDepthlineEdit.setGeometry(QtCore.QRect(80, 80, 51, 21))
-        self.iDepthlineEdit.setObjectName("iDepthlineEdit")
+        self.iDepthNumBox = QtWidgets.QSpinBox(self.centralwidget)
+        self.iDepthNumBox.setGeometry(QtCore.QRect(80,80,50,20))
+        self.iDepthNumBox.setObjectName("iDepthNumBox")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 210, 18))
@@ -73,21 +77,49 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        #connect slider and numbox
+        self.iDepthSlider.valueChanged.connect(self.handleiDepthSliderChange)
+        self.iDepthNumBox.valueChanged.connect(self.handleiDepthNumBoxChange)
+
+        #set up load inputs,
+       # self.actionSelect_Input_File.triggered.connect(self.get_input_file())
+      #  self.actionSelect_CF_File.triggered.connect(self.get_input_file())
+      #  self.actionLoad_frequency_map.connect(self.get_input_file())
+
+        #connect play button
+        self.playButton.clicked.connect(self.playClicked(self.inputFile, self.iDepthNumBox.value, self.cfFile, self.freqsFile))
+
+    def handleiDepthSliderChange(self, value):
+        self.iDepthNumBox.setValue(value)
+
+
+    def handleiDepthNumBoxChange(self, value):
+        self.iDepthSlider.setValue(value)
+
+    def get_input_file(self):
+        self.inputFile, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open input file",r"C:\\Users\\Kelvin Liu\\PycharmProjects\\CI_Simulator","input files (*.wav)")
+
+    def get_cf_file(self):
+        self.cfFile, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open CF file", "cf files (*.csv)")
+
+    def get_freqs_file(self):
+        self.freqsFile, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open frequency file", "frequecy files (*.csv)")
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Play"))
+        self.playButton.setText(_translate("MainWindow", "Play"))
         self.label.setText(_translate("MainWindow", "Insertion Depth (mm)"))
         self.menuFile.setTitle(_translate("MainWindow", "File"))
         self.actionSelect_Input_File.setText(_translate("MainWindow", "Load input file"))
         self.actionSelect_CF_File.setText(_translate("MainWindow", "Load CF file"))
         self.actionLoad_frequency_map.setText(_translate("MainWindow", "Load frequency map"))
 
-    def playClicked(self, file, insertionDepth, cfFile = "", freqsFile = ""):
-        output = executeAce(file, insertionDepth, cfFile = cfFile, freqsFile = freqsFile)
+    def playClicked(self, file, insertionDepth, cf = "", freqs = ""):
+        output = executeAce(file, self.iDepthNumBox.value(), cfFile = cf, freqsFile = freqs)
+        #what to do with output???
 
-def executeAce(input, insertionDepth, cfFile="", freqsFile=""):
+def executeAce( input, insertionDepth, cfFile="", freqsFile=""):
 
     # define useful paramters
     gain = 1  # input gain in dB, > 0
